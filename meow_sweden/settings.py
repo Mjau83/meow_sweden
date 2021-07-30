286260ab777e079f132fe0ca5f5c61fd6fa2447e
Mjau83 <mamoni.hemlin@gmail.com>
2021-07-30 14:02:29 +0000

    Added authentication backend from allauth

--- meow_sweden/settings.py
+++ meow_sweden/settings.py
@@ -67,6 +67,14 @@ TEMPLATES = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 WSGI_APPLICATION = 'meow_sweden.wsgi.application'
 
 
