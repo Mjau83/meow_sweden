6f53c72a6619d69cd71fa21d35e818e5458dbc03
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-02 17:46:00 +0000

    Added product info to products view

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .models import Product
 
 
 def all_products(request):
-    """ Returns a view of all products """
+    """ Returns a view with information about a individual product """
 
     products = Product.objects.all()
 
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_info(request, product_id):
+    """ Returns a view of all products """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_info.html', context)
