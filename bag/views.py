852f7ac7cb62d41bc6e18b15b68fb8ecd5698c66
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-07 11:56:27 +0000

    Created bag view

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """ Returns a view of the content of the bag """
+
+    return render(request, 'bag/bag.html')
