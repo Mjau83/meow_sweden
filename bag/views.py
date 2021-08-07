7f076631e2dadce2b01c19b29dd3d390d0347d23
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-07 12:15:15 +0000

    Added home link to main-nav.html

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 
-def index(request):
+def view_bag(request):
     """ Returns a view of the content of the bag """
 
     return render(request, 'bag/bag.html')
