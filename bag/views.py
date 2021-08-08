5db1f8cc5d18a100d1af5aaeeebdbbe83394de6b
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-08 20:21:13 +0000

    Added remove from bag views

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -36,7 +36,7 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag_items(request, item_id):
-    """ Add selected quantity of a product to shopping bag """
+    """ Adjust quantity of products in the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
 
@@ -50,11 +50,35 @@ def adjust_bag_items(request, item_id):
             bag[item_id]['items_by_color'][color] = quantity
         else:
             del bag[item_id]['items_by_color'][color]
+            if not bag[item_id]['items_by_color']:
+                bag.pop(item_id)
     else:
         if quantity > 0:
             bag[item_id] = quantity
         else:
-            bag.pop[item_id]
+            bag.pop(item_id)
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+def remove_bag_items(request, item_id):
+    """ Remove selected product from shopping bag """
+
+    try:
+        color = None
+        if 'catear_color' in request.POST:
+            color = request.POST['catear_color']
+        bag = request.session.get('bag', {})
+
+        if color:
+            del bag[item_id]['items_by_color'][color]
+            if not bag[item_id]['items_by_color']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500) 
