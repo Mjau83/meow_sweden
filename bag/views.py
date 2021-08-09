f4620a5afe50853491e90b9861e5f8bed0fadcf8
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-09 16:02:41 +0000

    Added message to remove bag items in bag views

--- bag/views.py
+++ bag/views.py
@@ -25,13 +25,13 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if color in bag[item_id]['items_by_color'].keys():
                 bag[item_id]['items_by_color'][color] += quantity
-                messages.success(request, f'Updated {color()} {product.name} quantity to {bag[item_id]["items_by_color"][color]}')
+                messages.success(request, f'Updated {color.upper()} {product.name} quantity to {bag[item_id]["items_by_color"][color]}')
             else:
                 bag[item_id]['items_by_color'][color] = quantity
-                messages.success(request, f'Added {color()} {product.name} to your Shopping Bag')
+                messages.success(request, f'Added {color.upper()} {product.name} to your Shopping Bag')
         else:
             bag[item_id] = {'items_by_color': {color: quantity}}
-            messages.success(request, f'Added {color()} {product.name} to your Shopping Bag')
+            messages.success(request, f'Added {color.upper()} {product.name} to your Shopping Bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
@@ -58,12 +58,12 @@ def adjust_bag_items(request, item_id):
     if color:
         if quantity > 0:
             bag[item_id]['items_by_color'][color] = quantity
-            messages.success(request, f'Updated {color()} {product.name} quantity to {bag[item_id]["items_by_color"][color]}')
+            messages.success(request, f'Updated {color.upper()} {product.name} quantity to {bag[item_id]["items_by_color"][color]}')
         else:
             del bag[item_id]['items_by_color'][color]
             if not bag[item_id]['items_by_color']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed {color()} {product.name} from your Shopping Bag')
+            messages.success(request, f'Removed {color.upper()} {product.name} from your Shopping Bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
@@ -80,6 +80,7 @@ def remove_bag_items(request, item_id):
     """ Remove selected product from shopping bag """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         color = None
         if 'catear_color' in request.POST:
             color = request.POST['catear_color']
@@ -89,11 +90,14 @@ def remove_bag_items(request, item_id):
             del bag[item_id]['items_by_color'][color]
             if not bag[item_id]['items_by_color']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed {color.upper()} {product.name} from your Shopping Bag')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your Shopping Bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'An Error occured removing item: {e}')
         return HttpResponse(status=500)
