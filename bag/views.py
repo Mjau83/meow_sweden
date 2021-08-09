18607f3c70a3757318da1f5ea42d69b9532bf0e1
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-09 12:26:03 +0000

    Added views for success message

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_bag(request):
@@ -10,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add selected quantity of a product to shopping bag """
 
+    products = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     color = None
@@ -30,6 +34,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'{product.name} was successfully added to your Shopping Bag!')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
