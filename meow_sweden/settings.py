f96019d76af2a8359e49abe7e1627a4be8ea6d1d
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-10 18:37:47 +0000

    Added public and secret key to settings to get them from env variables

--- meow_sweden/settings.py
+++ meow_sweden/settings.py
@@ -180,6 +180,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 FREE_DELIVERY_THRESHOLD = 55
 STANDARD_DELIVERY_PERCENTAGE = 15
 
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
