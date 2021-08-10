6bd61f6c34a4a3ef20a54a3a9936add45f26f432
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-10 12:59:48 +0000

    Added details fieldset to checkout template

--- meow_sweden/settings.py
+++ meow_sweden/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xul&7j=!*#e%2%*2xai1bth+#%3(jya(9!6f^ig#jsgx$(bnl*'
+SECRET_KEY = 'django-insecure-xul&7j=!*#e%2%*2' \
+ 'xai1bth+#%3(jya(9!6f^ig#jsgx$(bnl*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
