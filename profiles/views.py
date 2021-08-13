c91a274d6cba9cf316aaffea8b24314100515d65
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-13 18:59:27 +0000

    Updated profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
-from.forms import UserProfileForm
+from .forms import UserProfileForm
 
+from checkout.models import Order
 
 def profile(request):
     """ Display user profile. """
@@ -24,6 +25,22 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is you past order {order_number}. '
+        'A confirmation email was sent to you on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
