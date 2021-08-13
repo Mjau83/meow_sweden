b3ebded4838b010217096ab59369d1275f805717
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-13 11:57:57 +0000

    Connecting to AWS

--- products/models.py
+++ products/models.py
@@ -22,11 +22,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     catears_has_colors = models.BooleanField(default=False, null=True, blank=True)
-    quipu_color = models.BooleanField(default=False, null=True, blank=True)
+    quipu_color = models.ForeignKey('Color', null=True, blank=True, on_delete=models.SET_NULL)
     quipu_stonepearl = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    hex_code = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
