e2d38263f8916a52a6202c2f84b098a0a9354d5e
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-16 20:17:38 +0000

    Updated smartphone media query for navbar

--- products/models.py
+++ products/models.py
@@ -81,7 +81,7 @@ class Product(models.Model):
                                            on_delete=models.SET_NULL)
     qupiu_custom_form = models.ForeignKey('QuipuForm', null=True,
                                           on_delete=models.SET_NULL,
-                                          blank=False)
+                                          blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
