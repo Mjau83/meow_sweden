cb19c277d64cd7dd828bd1eea3b0dc3b9db0e54c
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-16 11:53:14 +0000

    Restored files from Gitpod shutdown

--- products/forms.py
+++ products/forms.py
@@ -16,8 +16,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-<<<<<<< HEAD
             field.widget.attrs['class'] = 'border-black rounded-0'
-=======
-            field.widget.attrs['class'] = 'border-black rounded-0'
->>>>>>> 105c4712cab185b954129b1e7e159af6382a4296
