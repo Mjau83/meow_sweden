027e8a9a1a63eec6572b0f1da1d3a172a5f6b32a
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-22 16:27:18 +0000

    Updated Product in products models

--- products/models.py
+++ products/models.py
@@ -90,12 +90,11 @@ class Product(models.Model):
     sku = models.CharField(max_length=200, null=True, blank=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
-    catears_has_colors = models.ForeignKey('CatEarColor', null=True,
-                                           blank=True,
-                                           on_delete=models.SET_NULL)
-    qupiu_custom_form = models.ForeignKey('QuipuForm', null=True,
-                                          on_delete=models.SET_NULL,
-                                          blank=True)
+    catear_color_option = models.ManyToManyField('CatEarColor')
+    catears_has_colors = models.BooleanField(default=False, null=True,
+                                             blank=True)
+    qupiu_custom_form = models.BooleanField(default=False, null=True,
+                                             blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
