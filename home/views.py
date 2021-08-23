0a4ce955d1a752b1c9ca7dce5cff79366e0aa3e1
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-23 00:40:53 +0000

    Added styling to about.html

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ def index(request):
     """ Returns index page view """
 
     return render(request, 'home/index.html')
+
+def about(request):
+    """ Returns about page view """
+    return render(request, 'home/about.html')
