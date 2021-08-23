5709f995f8c15bc4a2ba8f61313e1f03e5c87094
Mjau83 <mamoni.hemlin@gmail.com>
2021-08-23 11:19:06 +0000

    Updated add to bag function i bag views

--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,8 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add selected quantity of a product to shopping bag """
 
-    product = get_object_or_404(Product, pk=item_id)
+    product = get_object_or_404(Product, pk=item_id,
+                                catears_has_colors=color_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     color = None
@@ -29,21 +30,27 @@ def add_to_bag(request, item_id):
     if color:
         if item_id in list(bag.keys()):
             if color in bag[item_id]['items_by_color'].keys():
-                bag[item_id]['items_by_color'][color] += quantity
-                messages.success(request, f'Updated {color.upper()} {product.name} quantity to {bag[item_id] ["items_by_color"][color]}')
+                bag[item_id]['items_by_color'][color_id] += quantity
+                messages.success(request, f'Updated {color.upper()} '
+                                 + '{product.name} quantity to {bag[item_id] '
+                                 + '["items_by_color"][color]}')
             else:
-                bag[item_id]['items_by_color'][color] = quantity
-                messages.success(request, f'Added {color.upper()} {product.name} to your Shopping Bag')
+                bag[item_id]['items_by_color'][color_id] = quantity
+                messages.success(request, f'Added {color.upper()} '
+                                 + '{product.name} to your Shopping Bag')
         else:
-            bag[item_id] = {'items_by_color': {color: quantity}}
-            messages.success(request, f'Added {color.upper()} {product.name} to your Shopping Bag')
+            bag[item_id] = {'items_by_color': {color_id: quantity}}
+            messages.success(request, f'Added {color_id.upper()} '
+                             + '{product.name} to your Shopping Bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(request, f'Quantity of {product.name} is updated to {bag[item_id]}!')
+            messages.success(request, f'Quantity of {product.name} is ' 
+                             + 'updated to {bag[item_id]}!')
         else:
             bag[item_id] = quantity
-            messages.success(request, f'{product.name} was successfully added to your Shopping Bag!')
+            messages.success(request, f'{product.name} was successfully '
+                             + 'added to your Shopping Bag!')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -63,7 +70,8 @@ def adjust_bag_items(request, item_id):
     if color:
         if quantity > 0:
             bag[item_id]['items_by_color'][color] = quantity
-            messages.success(request, f'Updated {color.upper()} {product.name} quantity to {bag[item_id]["items_by_color"][color]}')
+            messages.success(request, f'Updated {color.upper()} '
+                             + '{product.name} quantity to {bag[item_id]["items_by_color"][color]}')
         else:
             del bag[item_id]['items_by_color'][color]
             if not bag[item_id]['items_by_color']:
